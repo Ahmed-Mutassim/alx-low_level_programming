814d4295d360392df247ae6a0e2ae61852dea03c
Ahmed-Mutassim <ahmedmuta346@gmail.com>
2022-10-15 11:24:51 -0700

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * The main func will print a statement by using puts
+ *
+ * The return vaule is 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
